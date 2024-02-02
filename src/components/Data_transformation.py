c8e49b4d326565da68859eca15b5ad8be299a409
Siddhudhole <shidhudhole358@gmail.com>
2024-02-02 21:27:50 +0530

    data transformation module is complete

---
+++ src/components/Data_transformation.py
@@ -0,0 +1,67 @@
+import os 
+import sys 
+import pandas as pd 
+import numpy as np 
+from src.Logger import logging
+from src.Exception import CustomException 
+from sklearn.pipeline import Pipeline 
+from sklearn.compose  import ColumnTransformer 
+from sklearn.impute import SimpleImputer 
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder  
+from dataclasses import dataclass 
+
+
+
+@dataclass 
+class DataTransformationCofig:
+    processor_file_path = os.path.join('artifacts','processor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.processor_config = DataTransformationCofig()
+
+    def get_processor(self):
+        try :
+            logging.info("Pipeline creation are started...")
+            num_cols = ['Area', 'BHK', 'Bathroom', 'Parking', 'Per_Sqft']
+            cat_cols = ['Furnishing', 'Status', 'Transaction', 'Type']
+            num_pip = Pipeline([('imputer',SimpleImputer(strategy='mean')),('scaler',StandardScaler())]) 
+            cat_pip = Pipeline([('imputer',SimpleImputer(strategy='most_frequent')),('encoder',OrdinalEncoder(categories='auto'))])
+            logging.info("Pipeline creation are completed...") 
+            logging.info("Preprocessor creation are started...")
+            preprocessor = ColumnTransformer([('num',num_pip,num_cols),('cat',cat_pip,cat_cols)]) 
+            logging.info("Preprocessor creation are completed...") 
+            return preprocessor 
+        
+        except CustomException as e:
+            logging.error(e)
+            raise CustomException(e,sys) 
+        
+    def Transformer(self,train_path,test_path):
+        try :
+            logging.info("Transformation is starting") 
+            train_df = pd.read_csv(train_path)
+            target_feature = 'Price' 
+            train_input_features = train_df.drop('Price',axis=1)
+            train_target_features = train_df[target_feature] 
+            logging.info('Preprocessor obtaining starting ')
+            preprocessor = self.get_processor() 
+            logging.info('Preprocessor obtained successfully')
+            train_input_arr = preprocessor.fit_transform(train_input_features)
+            train_arr = np.c_[np.array(train_input_arr),np.array(train_target_features)]
+            logging.info('Training data is tranformation successful')
+            test_df = pd.read_csv(test_path) 
+            test_input_features = test_df.drop('Price',axis=1) 
+            test_target_features = test_df[target_feature] 
+            test_input_arr = preprocessor.transform(test_input_features) 
+            test_arr = np.c_[np.array(test_input_arr),np.array(test_target_features)]
+            logging.info('Test data is tranformation successful') 
+            return train_arr,test_arr 
+
+        except CustomException as e:
+            logging.error(e)
+            raise CustomException(e,sys) 
+        
+
+
--- src/pipelines/Trainig_pip.py
+++ src/pipelines/Trainig_pip.py
@@ -4,14 +4,22 @@ from src.Logger import logging
 from src.Exception import CustomException 
 from src.components.Data_ingestion import DataIngestionConfig 
 from src.components.Data_ingestion import DataIngestion 
+from src.components.Data_transformation import DataTransformationCofig  
+from src.components.Data_transformation import DataTransformation  
 
 
 class Trainig_pipeline :
     try :
+        logging.info("Training pipeline starting") 
+        logging.info('data loading module starting')
         data_ingestion = DataIngestion()
         train_path,test_path = data_ingestion.load_data() 
-        print(train_path)
-        print(test_path)
+        logging.info('data loading successful')
+        logging.info('data transformation module starting') 
+        data_transformer = DataTransformation()
+        train_arr,test_arr = data_transformer.Transformer(train_path,test_path) 
+        logging.info('data transformation successful')  
+        print('data transformation successful') 
 
     except Exception as e :
         logging.error(e)
